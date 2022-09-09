3d3718939dac0968384214962ff163730d06ea30
javiinostroza <jminostroza@uc.cl>
2022-09-09 16:58:49 -0400

    Clean code

--- subrutinas.go
+++ subrutinas.go
@@ -1,5 +1,4 @@
 package main
-
 import (
 	"fmt"
 	"os"
@@ -8,11 +7,14 @@ import (
 	str "strings"
 )
 
-func check(e error) { // Based on https://gobyexample.com/writing-files
+
+func check(e error) { 
+	// Based on https://gobyexample.com/writing-files
     if e != nil {
         panic(e)
     }
 }
+
 
 func mergeSort(items []int) []int {
 	/*
@@ -25,6 +27,7 @@ func mergeSort(items []int) []int {
     second := mergeSort(items[len(items)/2:])
     return merge(first, second)
 }
+
 
 func merge(a []int, b []int) []int {
 	/*
@@ -51,21 +54,20 @@ func merge(a []int, b []int) []int {
     return final
 }
 
+
 func mergeRoutine(list_ []int, channel chan []int) {
-	fmt.Println("en turnina ", list_)
 	list_ = mergeSort(list_)
-	fmt.Println("sorted: ", list_)
 	channel <- list_
 }
 
+
 func main() {
+
 	array := [16] int{}
 	reader := bufio.NewReader(os.Stdin)
 	for i := 0; i < 16; i++ {
 		text, _ := reader.ReadString('\n')
 		num := str.TrimSpace(text)
-		fmt.Println("num: ", num)
-
 		intNum, err := conv.Atoi(num)
 		check(err)
 		array[i] = intNum
@@ -75,7 +77,6 @@ func main() {
 	channel2 := make(chan []int)
 	channel3 := make(chan []int)
 	channel4 := make(chan []int)
-
 
 	go mergeRoutine(array[:4], channel1)
 	go mergeRoutine(array[4:8], channel2)
@@ -87,34 +88,16 @@ func main() {
 	subArr3 := <- channel3
 	subArr4 := <- channel4
 
-	list1 := []int{}
-	list2 := []int{}
-	fmt.Println(subArr1)
-	fmt.Println(subArr2)
-	fmt.Println(subArr3)
-	fmt.Println(subArr4)
+	go mergeRoutine(append(subArr1, subArr2...), channel1)
+	go mergeRoutine(append(subArr3, subArr4...), channel2)
 
-
-	
-	list1 = append(list1, subArr1...)
-	list1 = append(list1, subArr2...)
-
-	list2 = append(list2, subArr3...)
-	list2 = append(list2, subArr4...)
-
-	fmt.Println("list1 merged: ", list1)
-	fmt.Println("list2 merged: ", list2)
-
-	go mergeRoutine(list1, channel1)
-	go mergeRoutine(list2, channel2)
-	fmt.Println("Despues de los append")
 	subArr1 = <- channel1
 	subArr2 = <- channel2
 
 	go mergeRoutine(append(subArr1, subArr2...), channel1)
+
 	sorted_list := <- channel1
 
-	fmt.Println("Final: ", sorted_list)
-
+	fmt.Println(sorted_list)
 }
 
